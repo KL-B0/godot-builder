c456de1ab214e32b235dd1018dd3f8d9eb24df92
KL-B0 <59870373+KL-B0@users.noreply.github.com>
2023-03-08 09:59:59 +0100

    Setup ncc
    Configure ncc compiler

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,16 @@
-"use strict";
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
 // 1. Create directory
 // 2. Build project for plaform X
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 // 1. Create directory
 // 2. Build project for plaform X
+
+

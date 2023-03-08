90e12bbb98c77f30156689b3887934be40dec97c
KL-B0 <59870373+KL-B0@users.noreply.github.com>
2023-03-08 12:09:05 +0100

    Add sample code
    Add some sample code to the action

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 // 1. Create directory
 // 2. Build project for plaform X
 
+import * as core from '@actions/core';
 
+function main() {
+  try {
+    console.log(core.getInput('godot-version'));
+  } catch (error) {
+    core.setFailed((error as Error).message);
+  }
+}
+
+main();

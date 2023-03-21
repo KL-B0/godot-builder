698d22861b99138d2772ecb77f2025df481958b6
KLB0 <capalbo.lorenzo@tutanota.com>
2023-03-21 20:48:19 +0100

    Add run docker command
    Add function to run docker containers

--- src/godot-builder/docker.ts
+++ src/godot-builder/docker.ts
@@ -1,38 +1,46 @@
 import {existsSync, mkdirSync} from 'node:fs'
 import path from 'node:path'
-
-import Image from './image'
+import {getExecOutput, ExecOptions} from '@actions/exec'
 
 import {getActionFolder, getWorkspace} from './action'
 import BuildConfig from './build-config'
+import Image from './image'
 
-//export async function run(image: string, parameters: any, entryPoint): void {
+import * as core from '@actions/core'
 
-//}
-
-export function getUnixCommand(
+export async function run(
   image: Image,
   config: BuildConfig,
-  debug: boolean,
-  runnerPath: string
-): string {
-  const githubHome = path.join(runnerPath, '_github_home')
+  debug = false,
+  options: ExecOptions | undefined = undefined
+): Promise<number> {
+  const githubHome = path.join('', '_github_home')
   if (!existsSync(githubHome)) mkdirSync(githubHome)
-  const githubWorkflow = path.join(runnerPath, '_github_workflow')
+  const githubWorkflow = path.join('', '_github_workflow')
   if (!existsSync(githubWorkflow)) mkdirSync(githubWorkflow)
 
-  return `docker run \
-    -w /github/workspace/ \
-    --rm \
-    -e GITHUB_WORKSPACE=/github/workspace \
-    -e EXPORT_PRESET=${config.exportPreset} \
-    -e EXPORT_PATH=${config.exportPath} \
-    -e EXPORT_NAME=${config.exportName} \
-    ${debug ? `-e DEBUG=1` : ``} \
-    -v "${githubHome}":"/root:z" \
-    -v "${githubWorkflow}":"/github/workflow:z" \
-    -v "${getWorkspace()}":"/github/workspace:z" \
-    -v "${getActionFolder()}/runners/linux/build.sh:/build.sh:z" \
-    --entrypoint /build.sh \
-    ${image.generateTag()}`
+  const result = await getExecOutput(
+    'docker run',
+    [
+      '-w /github/workspace/',
+      '-rm',
+      '-e GITHUB_WORKSPACE=/github/workspace',
+      `-e PROJECT_PATH = ${config.projectPath}`,
+      `-e EXPORT_PRESET = ${config.exportPreset}`,
+      `-e EXPORT_PATH = ${config.exportPath}`,
+      `-e EXPORT_NAME = ${config.exportName}`,
+      `${debug ? '-e DEBUG=1' : ''}`,
+      `-v "${githubHome}": "/root:z"`,
+      `-v "${githubWorkflow}": "/github/workflow:z"`,
+      `-v "${getWorkspace()}": "/github/workspace:z"`,
+      `-v "${getActionFolder()}/runners/linux/build.sh:/build.sh:z"`,
+      `--entrypoint /build.sh`,
+      `${image.generateTag()}`
+    ],
+    options
+  )
+
+  core.debug(result.stdout)
+
+  return 0
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import * as core from '@actions/core'
 
 import {BuildConfig, Image} from './godot-builder'
+import {run} from './godot-builder/docker'
 
 async function main() {
   try {
     const buildConfig = new BuildConfig()
-    const imageTag = new Image(buildConfig)
-    console.log(imageTag.generateTag())
+    const image = new Image(buildConfig)
+
+    await run(image, buildConfig)
   } catch (error) {
     core.error((error as Error).message)
   }

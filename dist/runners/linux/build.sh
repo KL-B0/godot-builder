25bbd06d2b81a66711c333d6b353a1b619a1c05e
KL-B0 <59870373+KL-B0@users.noreply.github.com>
2023-03-21 11:47:30 +0100

    Update build script
    Update build script to consider both export name and path

--- dist/runners/linux/build.sh
+++ dist/runners/linux/build.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ $DEBUG ]]; then
-  godot --headless --export-debug $EXPORT_PRESET $EXPORT_PATH
+if [[ ${DEBUG:+1} ]] ; then
+  godot --headless --export-debug $EXPORT_PRESET $EXPORT_PATH/$EXPORT_NAME
 else
-  godot --headless --export-release $EXPORT_PRESET $EXPORT_PATH
+  godot --headless --export-release $EXPORT_PRESET $EXPORT_PATH/$EXPORT_NAME
 fi

10435a4356b5a883c61762b57e28e411a8f7e1d2
KL-B0 <59870373+KL-B0@users.noreply.github.com>
2023-03-22 12:19:15 +0100

    Update build script
    Create export path if not existing

--- dist/runners/linux/build.sh
+++ dist/runners/linux/build.sh
@@ -3,6 +3,12 @@
 # Move to project path
 cd $PROJECT_PATH
 
+# Create export directory, if it doesn't exist
+if [[ ! -d $EXPORT_PATH ]]; then
+  mkdir -p $EXPORT_PATH
+fi
+
+# Export the game
 if [[ ${DEBUG:+1} ]] ; then
   godot --headless --export-debug "$EXPORT_PRESET" $EXPORT_PATH/$EXPORT_NAME
 else
